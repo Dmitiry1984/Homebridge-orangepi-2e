eec20b53cff302a52f2b702b3cf8c3d323dc5bc2
Baryshnikov Dmitriy <46015385+Dmitiry1984@users.noreply.github.com>
2018-12-21 14:59:19 +0700

    Add files via upload
    Got from https://github.com/fantomnotabene/homebridge_setup_script 
    - changed with last node.js version
    - added homebridge startup

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+sudo apt-get install -y nodejs libavahi-compat-libdnssd-dev
+mkdir ~/.homebridge && cp config.json ~/.homebridge
+
+sudo npm i -g --unsafe-perm homebridge homebridge-config-ui-x
+
+sudo cp homebridge.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable homebridge
+sudo systemctl start homebridge
